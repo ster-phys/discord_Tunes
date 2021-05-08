0b844e216f786c9d1e4baa97acc08c0dc1564848
ster <ster.physics@gmail.com>
2021-05-08 19:25:47 +0900

    追加：mainにimportするファイルを選択

---
+++ Tunes/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    "join",
+    "leave",
+    "play",
+    "stop",
+]
+
+from .join import join
+from .leave import leave
+from .play import play
+from .stop import stop
